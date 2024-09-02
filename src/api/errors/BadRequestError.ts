62fdb4131a810412662764331494b7f92d229ee7
Guido Menendez <guidomenendez0@gmail.com>
2024-09-02 14:54:15 -0300

    add docs

--- src/api/errors/BadRequestError.ts
+++ src/api/errors/BadRequestError.ts
@@ -1,3 +1,9 @@
+/**
+ * Represents an error that occurs when a request is bad formatted.
+ *
+ * This class extends the built-in `Error` class and sets the prototype to `BadRequestError`
+ * to ensure proper inheritance in custom error handling scenarios.
+ */
 export class BadRequestError extends Error {
     constructor(message: string) {
         super(message);
